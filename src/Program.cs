e99abc588061a04f57b016600dca058c0356d7c8
TreyHendon <trey.hendon@smithtech.com>
2021-09-22 15:01:45 -0400

    Add deep logging for Kubernetes debugging

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace KubeStatus
 {
@@ -12,6 +13,13 @@ namespace KubeStatus
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Trace);
+                    logging.AddFilter("Microsoft.AspNetCore.Http.Connections", LogLevel.Trace);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
