a132b9370e8c0b4baedfa7803198804023dacba8
TreyHendon <trey.hendon@smithtech.com>
2021-10-08 11:03:53 -0400

    Update to KubernetesClient 6.0.11

--- src/Data/NamespaceService.cs
+++ src/Data/NamespaceService.cs
@@ -9,23 +9,9 @@ namespace KubeStatus.Data
     {
         public async Task<V1NamespaceList> GetAllNamespacesAsync()
         {
-            Console.WriteLine("Fetching Client");
             var client = Helper.GetKubernetesClient();
-            Console.WriteLine("Client Fetched");
-
-            Console.WriteLine("Fetching Namespaces");
+            
             var namespaces = client.ListNamespace();
-            Console.WriteLine("Namespaces Fetched");
-
-            foreach (var item in namespaces.Items)
-            {
-                Console.WriteLine(item.Metadata.Name);
-            }
-
-            if (namespaces.Items.Count == 0)
-            {
-                Console.WriteLine("Empty!");
-            }
 
             return await Task.FromResult(namespaces);
         }
