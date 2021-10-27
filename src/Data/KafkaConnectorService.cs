35440a135d25a30f4bbdc6aafc647cac8af0f648
TreyHendon <trey.hendon@smithtech.com>
2021-10-27 16:43:38 -0400

    Fix null logic and test before taking to string

--- src/Data/KafkaConnectorService.cs
+++ src/Data/KafkaConnectorService.cs
@@ -63,23 +63,26 @@ namespace KubeStatus.Data
                 {
                     connectorState = clusterCustomObject.SelectToken("status.connectorStatus.connector.state").ToString();
 
-                    if (clusterCustomObject.SelectToken("status.connectorStatus", errorWhenNoMatch: false) != null && clusterCustomObject.SelectToken("status.connectorStatus").Children().Any(t => t.Path.Contains("status.connectorStatus.tasks[0]")))
+                    var taskStateObj = clusterCustomObject.SelectToken("status.connectorStatus.tasks[0].state", false);
+                    if (taskStateObj != null)
                     {
-                        taskState = clusterCustomObject.SelectToken("status.connectorStatus.tasks[0].state", errorWhenNoMatch: false).ToString();
+                        taskState = taskStateObj.ToString();
                     }
 
-                    if (clusterCustomObject.SelectToken("status.connectorStatus.tasks[0]", errorWhenNoMatch: false) != null && clusterCustomObject.SelectToken("status.connectorStatus.tasks[0]").Children().Any(t => t.Path.Contains("status.connectorStatus.tasks[0].trace")))
+                    var taskTraceObj = clusterCustomObject.SelectToken("status.connectorStatus.tasks[0].trace", false);
+                    if (taskTraceObj != null)
                     {
-                        taskTrace = clusterCustomObject.SelectToken("status.connectorStatus.tasks[0].trace", errorWhenNoMatch: false).ToString();
+                        taskTrace = taskTraceObj.ToString();
                     }
                 }
                 else
                 {
                     connectorState = clusterCustomObject.SelectToken("status.conditions[0].type").ToString();
 
-                    if (clusterCustomObject.SelectToken("status.conditions[0]", errorWhenNoMatch: false) != null && clusterCustomObject.SelectToken("status.conditions[0]").Children().Any(t => t.Path.Contains("status.conditions[0].message")))
+                    var taskTraceObj = clusterCustomObject.SelectToken("status.conditions[0].message", false);
+                    if (taskTraceObj != null)
                     {
-                        taskTrace = clusterCustomObject.SelectToken("status.conditions[0].message", errorWhenNoMatch: false).ToString();
+                        taskTrace = taskTraceObj.ToString();
                     }
                 }
 
