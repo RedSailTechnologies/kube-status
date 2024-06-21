0c021bd60c848a2fd60efbf61f61b39dfdca64ad
treyhendon <treyhendon@gmail.com>
2024-06-21 15:25:48 -0400

    Update format helper script

--- dotnet-format.sh
+++ dotnet-format.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-mkdir -p bin
-
-FILE=./bin/dotnet-format.exe
-if [[ ! -f "$FILE" ]]; then
-    echo "dotnet-format not found, installing..."
-    dotnet tool install --tool-path ./bin dotnet-format
-fi
-
-$FILE ./src --folder
+projects=$(find "." -type f -name "*.csproj")
+for project in $projects; do
+    echo "Formatting $project..."
+    dotnet format $project
+    echo ""
+done
